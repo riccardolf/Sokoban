31ec5a2262cd794e8c13926462721caf965e8315
check96 <donadio24@gmail.com>
2017-02-22 16:14:50 +0100

    Add files via upload

---
+++ sokoban/sokoban/GestoreGioco.h
@@ -0,0 +1,40 @@
+#ifndef GESTOREGIOCO_H
+#define GESTOREGIOCO_H
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_image.h>
+#include<allegro5/allegro_audio.h>
+#include<allegro5/allegro_native_dialog.h>
+#include<allegro5/allegro_primitives.h>
+#include"Livello.h"
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+class GestoreGioco
+{
+	public:
+		GestoreGioco();
+		~GestoreGioco();
+		void Modalita();
+		void creaLivello(int liv);
+
+	private:
+		void inizializzaAllegro();
+		int numLivelli;
+		vector<Cassa*> casse;
+		vector<Muro*> muri;
+		int** posCasseXi;
+		int** posCasseXf;
+		int** posCasseYi;
+		int** posCasseYf;
+		int** posMuriX;
+		int** posMuriY;
+		int* posGiocatoreX;
+		int* posGiocatoreY;
+		int* numCasse;
+		int* numMuri;
+};
+
+
+#endif
